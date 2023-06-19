98a235dea98ca5f02e567b5df0a728c412a4288e
timtim <youcantwitter@gmail.com>
2023-06-19 22:47:48 +0900

    Greetings added

--- Ex01/Dice Game.go
+++ Ex01/Dice Game.go
@@ -1,21 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"math/rand"
-	"time"
+	"os"
 )
 
 func main() {
-	// Seed the random number generator with the current time
-	rand.Seed(time.Now().UnixNano())
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("What is your name? \n> ")
 
-	// Generate two random numbers from 1 to 6
-	die1 := rand.Intn(6) + 1
-	die2 := rand.Intn(6) + 1
+	name, _ := reader.ReadString('\n')
 
-	fmt.Println("Rolling dice...")
-	fmt.Println("Die 1:", die1)
-	fmt.Println("Die 2:", die2)
-	fmt.Println("Total value:", die1+die2)
+	// Print a greeting message
+	fmt.Printf("Hello, %s!\n", name)
 }
